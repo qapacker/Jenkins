03b2f92fe0d9ab7d875702ef6e10c213dd773e3f
mateo <mateo.gil@i2cat.net>
2025-05-13 16:51:36 +0200

    cambios en archivo api.py

--- app/api.py
+++ app/api.py
@@ -1,7 +1,5 @@
 import http.client
-
 from flask import Flask
-
 from app import util
 from app.calc import Calculator
 
@@ -31,3 +29,31 @@ def substract(op_1, op_2):
         return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/multiply/<op_1>/<op_2>", methods=["GET"])
+def multiply(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.multiply(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+@api_application.route("/calc/divide/<op_1>/<op_2>", methods=["GET"])
+def divide(op_1, op_2):
+    try:
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        if num_2 == 0:
+            raise ValueError("Cannot divide by zero")
+        return ("{}".format(CALCULATOR.divide(num_1, num_2)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+    except ValueError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+
+# Bloque para ejecutar la aplicación
+if __name__ == "__main__":
+    api_application.run(debug=True)
+
